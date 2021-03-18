b6dd9e6cb0319ca1eb5bc9848688b57d98ab6408
Moreno Andrade <laforetmoreno@gmail.com>
2021-03-18 16:11:38 -0300

    Add ts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,21 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from './styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+const Home = ({ title }: Props) => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Moreno!</a>
         </h1>
 
         <p className={styles.description}>
@@ -63,3 +67,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
