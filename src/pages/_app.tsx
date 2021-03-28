a6555ccc9dea57432306c349ecb9c2a2508c0368
Moreno Andrade <laforetmoreno@gmail.com>
2021-03-28 19:37:52 -0300

    Add next-pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,11 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        {/* <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" /> */}
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
+        <title>React Avançado</title>
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and Styled Components"
